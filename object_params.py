4c311b500ba7cf5328f2e091999965e34a159544
chw3k5 <chw3k5@gmail.com>
2021-06-09 17:35:32 -0500

    Update for front and and backend teams. Data uploaded to schemes that are not used by the website and then only copied after verification. There is a new table the is a curated set of parameters. 1 parameter per star. This table has been wrapped by query engine as an example.

--- object_params.py
+++ object_params.py
@@ -4,17 +4,18 @@ from autostar.simbad_query import StarDict
 
 class ObjectParams(StarDict):
     def __setitem__(self, key, value):
+        key = str(key).lower().replace(' ', '')
         if not self.__contains__(key):
             if isinstance(value, set):
-                self.data[str(key)] = value
+                self.data[key] = value
             elif isinstance(value, SingleParam):
-                self.data[str(key)] = {value}
+                self.data[key] = {value}
             else:
                 raise ValueError("SingleParam tuple or set is required")
         if isinstance(value, set):
-            self.data[str(key)] |= value
+            self.data[key] |= value
         elif isinstance(value, SingleParam):
-            self.data[str(key)].add(value)
+            self.data[key].add(value)
         else:
             raise ValueError("SingleParam tuple or set is required")
 
